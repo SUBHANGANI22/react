f19d5b062d515a88a8241af48d276d86c5b3946b
SUBHANGANI JHA <72139942+SUBHANGANI22@users.noreply.github.com>
2024-07-22 08:16:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import TaskManager from './components/TaskManager';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
